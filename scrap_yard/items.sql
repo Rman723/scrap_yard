30f00a82d606dcd8b23d5f6add1eff41f1f9cad9
Rman723 <rhett.thacker@gmail.com>
2020-10-04 20:25:54 -0600

    Add files via upload

---
+++ scrap_yard/items.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES -- chech if you 
+    ('zink', 'Zink', 1, 3, 1),
+    ('battery_acid', 'Battery Acid', 1, 3, 1);
+    ('wires', 'Wires', 1, 3, 1);
+    ('steel', 'Steel', 1, 3, 1);
